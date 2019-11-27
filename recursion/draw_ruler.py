cd7132414473dd8ffd7cb83a5f4f6b948c74b979
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-27 18:50:42 +0100

    Implemented draw interval

--- recursion/draw_ruler.py
+++ recursion/draw_ruler.py
@@ -5,3 +5,10 @@ def draw_line(tick_length, tick_label=" "):
         line += " " + tick_label
 
     print(line)
+
+
+def draw_interval(center_length):
+    if center_length > 0:
+        draw_interval(center_length - 1)
+        draw_line(center_length)
+        draw_interval(center_length - 1)
