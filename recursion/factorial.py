73eb7f327f7dfc700ed646dc63590743c6783848
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-27 18:38:24 +0100

    Implemented the pseudocode

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,5 +1,7 @@
 def factorial(n):
     # state a base case of if n equals zero
+    if n == 0:
         # return 1
+        return 1
     # return n multiplied by factorial called with n - 1
-    pass
+    return n * factorial(n - 1)
