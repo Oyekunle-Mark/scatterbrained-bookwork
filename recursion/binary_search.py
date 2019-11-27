db2b53956fed9b6a4ea38f2f8b21b1d0e4df6fcf
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-27 19:07:33 +0100

    Implemented the first pass

--- recursion/binary_search.py
+++ recursion/binary_search.py
@@ -1,11 +1,20 @@
 def binary_search(arr, target, low, high):
     # find the mid of high and low
+    mid = (low + high) // 2
     # if item at index is target return
+    if arr[mid] == target:
+        return True
     # if state a base case of low is greater than or equals high
+    elif low >= high:
         # return false
+        return False
     # otherwise
-        # if target is greater than mid of arr
+    else:
+        # if mid of arr is greater than target
+        if arr[mid] > target:
             # recursively call binarysearch with low and mid
+            binary_search(arr, target, low, mid)
         # otherwise
+        else:
             # recursively call binarysearch with mid and high
-    pass
+            binary_search(arr, target, mid, high)
