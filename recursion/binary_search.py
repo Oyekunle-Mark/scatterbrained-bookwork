c7370a8535427780bee406fd2d422b897ad91448
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-27 19:12:13 +0100

    Fixed bug and added test cases

--- recursion/binary_search.py
+++ recursion/binary_search.py
@@ -13,8 +13,12 @@ def binary_search(arr, target, low, high):
         # if mid of arr is greater than target
         if arr[mid] > target:
             # recursively call binarysearch with low and mid
-            binary_search(arr, target, low, mid)
+            return binary_search(arr, target, low, mid - 1)
         # otherwise
         else:
             # recursively call binarysearch with mid and high
-            binary_search(arr, target, mid, high)
+            return binary_search(arr, target, mid + 1, high)
+
+
+arr = [1, 3, 4, 6, 8, 12, 45]
+print(binary_search(arr, 42, 0, len(arr)))
