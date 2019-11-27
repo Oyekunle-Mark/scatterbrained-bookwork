b5434f5871248f38d67721308d52172efcfac688
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-27 19:04:28 +0100

    Binary search psudocode

--- recursion/binary_search.py
+++ recursion/binary_search.py
@@ -1,2 +1,11 @@
 def binary_search(arr, target, low, high):
+    # find the mid of high and low
+    # if item at index is target return
+    # if state a base case of low is greater than or equals high
+        # return false
+    # otherwise
+        # if target is greater than mid of arr
+            # recursively call binarysearch with low and mid
+        # otherwise
+            # recursively call binarysearch with mid and high
     pass
