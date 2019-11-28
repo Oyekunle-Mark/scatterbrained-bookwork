34127f10fbb798bce2b17d7ad3b665c91ca6ea33
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-28 09:56:13 +0100

    Implemented the disk usage function

--- recursion/file_systems.py
+++ recursion/file_systems.py
@@ -1,2 +1,14 @@
+import os
+
+
 def disk_usage(path):
-    pass
+    total = os.path.getsize()
+
+    if os.path.isdir(path):
+        for f in os.listdir(path):
+            full_path = os.path.join(path, f)
+            total += disk_usage(full_path)
+
+    print("{0:<7}".format(total), path)
+
+    return total
