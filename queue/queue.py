66201404511c45d7c5517c8a218b4870e63acf3a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-05 18:35:02 +0100

    Implemented the constructor

--- queue/queue.py
+++ queue/queue.py
@@ -4,4 +4,11 @@ class Empty(Exception):
 
 
 class ArrayQueue:
-    pass
+    """FIFO queue implementation using a Python list as underlying storage."""
+    _CAPACITY = 10
+
+    def __init__(self):
+        """Create an empty queue"""
+        self._storage = [None] * self._CAPACITY
+        self.size = 0
+        self.front = 0
